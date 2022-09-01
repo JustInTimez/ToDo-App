d1ba90cf9a6a0839895c52f17e1700a65af4ac36
JustInTimez <just.oost@gmail.com>
2022-09-01 13:58:53 +0200

    Structure and logic
    Updated HTML for form, and added logic to use form info. Have not yet got it printing to the DOM

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -9,14 +9,14 @@ function addToDo (text) {
         id: Date.now(),
     }
     todoItems.push(todo);
-    console.log(todoItems);
+    renderTodo(todo);
 };
 
 const form = document.querySelector('.todoForm');
 
 // Added event listener for form submit, and added some functionality to remove any white-space if left in the input field
 form.addEventListener('submit', event => {
-    event.preventDefault();
+    event.preventDefault();                 // Prevent form from trying to submit to server
     const input = document.querySelector('.todoInput');
     const text = input.value.trim();
     if (text !== '') {
@@ -25,3 +25,22 @@ form.addEventListener('submit', event => {
         input.focus();
     }
 });
+
+function renderTasks(todo) {
+    const list = document.querySelector('.todo-list');
+    const isChecked = todo.checked ? 'completed': '';
+    const liNode = document.createElement('li');
+    liNode.setAttribute('class', `todo-item ${isChecked}`);
+    liNode.setAttribute('data-key', todo.id);
+    liNode.innerHTML = `
+    <input id="${todo.id}" type="checkbox" />
+    <label for="${todo.id}" class="tick js-tick"></label>
+    <span>${todo.text}</span>
+    <button class="delete-todo js-delete-todo">
+    <svg><use href=#delete-icon"></use></svg>
+    </button>
+    `;
+    // Append this element to the dom by using list as reference
+    list.append(liNode);
+
+}
