c103292f24648537109953ffae688a1c5d8565a0
JustInTimez <just.oost@gmail.com>
2022-09-05 15:45:13 +0200

    Built main app and functionality
    I was not happy with what I had been building, so decided to do something else. Inc show list of Pending and Completed tasks

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,134 +1,106 @@
-// This array will hold the todo items
-let todoItems = [];
+const taskInput = document.querySelector(".task-input input"),
+    filters = document.querySelectorAll(".filters span"),
+    clearAll = document.querySelector(".clear-btn"),
+    taskBox = document.querySelector(".task-box");
 
-// A function that will create object based on user input and push to the array above
-function addToDo(text, descrip) {
-    const todo = {
-        text,
-        descrip,
-        checked: false,
-        id: Date.now(),
-    }
-    todoItems.push(todo);
-    renderTasks(todo);
-};
+let editId,
+    isEditTask = false,
+    todos = JSON.parse(localStorage.getItem("todo-list"));
 
-// Function that takes in the event listener for the checkmark click event
-function toggleComplete(key) {
-    const index = todoItems.findIndex(item => item.id === Number(key));
-    todoItems[index].checked = !todoItems[index].checked;
-    renderTasks(todoItems[index]);
-};
-
-// Function to edit task
-const editButton = document.querySelector('js-edit-todo');
-function editList(edit) {
-    editButton.value = edit;
-
-    text.value = todoItems[edit];
-
-
-}
-
-// Function to delete items from DOM
-function deleteTodo(key) {
-    localStorage.setItem('rememberData', JSON.stringify(todoItems));
-    const index = todoItems.findIndex(item => item.id === Number(key));
-    const todo = {
-        deleted: true,
-        ...todoItems[index]
-    };
-    todoItems = todoItems.filter(item => item.id !== Number(key));
-    renderTasks(todo);
-}
-
-const form = document.querySelector('.todoForm');
-
-// Added event listener for form submit, and added some functionality to remove any white-space if left in the input field
-form.addEventListener('submit', event => {
-    event.preventDefault();                 // Prevent form from trying to submit to server
-    const input = document.querySelector('.todoInput');
-    const inputDescrip = document.querySelector('.todoDescrip');
-    const text = input.value.trim();
-    const descrip = inputDescrip.value.trim();
-    if (text && descrip !== '') {
-        addToDo(text, descrip);
-        input.value = '';
-        input.focus();
-    }
+filters.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelector("span.active").classList.remove("active");
+        btn.classList.add("active");
+        showTodo(btn.id);
+    });
 });
 
-function renderTasks(todo) {
-    localStorage.setItem('rememberData', JSON.stringify(todoItems));
-    const list = document.querySelector('.todo-list');
-    const item = document.querySelector(`[data-key='${todo.id}']`);
-    if (todo.deleted) {
-        item.remove();
-        if (todoItems.length === 0) list.innerHTML = '';
-        return;
-    }
-    const isChecked = todo.checked ? 'completed' : '';
-    const divNode = createNode(todo, isChecked);
-    // If item is in DOM already, either replace it so that no duplication occures or append to end of list
-    if (item) {
-        list.replaceChild(divNode, item);
-    } else {
-        list.append(divNode);
-    }
-}
-
-function createNode(todo, isChecked) {
-    const divNode = document.createElement('div');
-    divNode.setAttribute('class', `todo-item accordion-item ${isChecked}`);
-    divNode.setAttribute('data-key', todo.id);
-    divNode.innerHTML = `
-    <div id="dropDownContainer">
-    <input id="${todo.id}" class="accordion-button form-check-input program_checkbox" type="checkbox" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-    <label for="${todo.id}" class="tick js-tick"></label>
-    <div class="accordion" id="accordionPanelsStayOpen">
-      <h2 class="accordion accordion-header" id="headingOne">
-        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            <span>${todo.text}</span>
-        </button>
-      </h2>
-      </div>
-      <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-        <div class="accordion-body description">
-            <span>${todo.descrip}</span>
-        </div>
-      </div>
-    </div>
-    <button class="edit-todo js-edit-todo">
-        <svg><use href=#edit-icon"></use></svg>
-    </button>
-    <button class="delete-todo js-delete-todo" onclick()>
-        <svg><use href=#delete-icon"></use></svg>
-    </button>
-    `;
-    return divNode;
-}
-
-// Listen for and apply checkmark as well as delete button listener
-
-const list = document.querySelector('.js-todo-list');
-list.addEventListener('click', event => {
-    if (event.target.classList.contains('js-tick')) {
-        const itemKey = event.target.parentElement.dataset.key;
-        toggleComplete(itemKey);
-    }
-    if (event.target.classList.contains('js-delete-todo')) {
-        const itemKey = event.target.parentElement.dataset.key;
-        deleteTodo(itemKey);
-    }
-});
-
-// Listener to Access LocalStorage and update the DOM if necessary
-document.addEventListener('DOMContentLoaded', () => {
-    const loadData = localStorage.getItem('rememberData');
-    if (loadData) {
-        todoItems = JSON.parse(loadData);
-        todoItems.forEach(r => {
-            renderTasks(r);
+function showTodo(filter) {
+    let liTag = "";
+    if (todos) {
+        todos.forEach((todo, id) => {
+            let completed = todo.status == "completed" ? "checked" : "";
+            if (filter == todo.status || filter == "all") {
+                liTag += `<li class="task">
+                            <label for="${id}">
+                                <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
+                                <p class="${completed}">${todo.name}</p>
+                            </label>
+                            <div class="settings">
+                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                                <ul class="task-menu">
+                                    <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
+                                    <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
+                                </ul>
+                            </div>
+                        </li>`;
+            }
         });
+    }
+    taskBox.innerHTML = liTag || `<span>No tasks - please create one to get started!</span>`;
+    let checkTask = taskBox.querySelectorAll(".task");
+    !checkTask.length ? clearAll.classList.remove("active") : clearAll.classList.add("active");
+    taskBox.offsetHeight >= 300 ? taskBox.classList.add("overflow") : taskBox.classList.remove("overflow");
+}
+showTodo("all");
+
+function showMenu(selectedTask) {
+    let menuDiv = selectedTask.parentElement.lastElementChild;
+    menuDiv.classList.add("show");
+    document.addEventListener("click", e => {
+        if (e.target.tagName != "I" || e.target != selectedTask) {
+            menuDiv.classList.remove("show");
+        }
+    });
+}
+
+function updateStatus(selectedTask) {
+    let taskName = selectedTask.parentElement.lastElementChild;
+    if (selectedTask.checked) {
+        taskName.classList.add("checked");
+        todos[selectedTask.id].status = "completed";
+    } else {
+        taskName.classList.remove("checked");
+        todos[selectedTask.id].status = "pending";
+    }
+    localStorage.setItem("todo-list", JSON.stringify(todos))
+}
+
+function editTask(taskId, textName) {
+    editId = taskId;
+    isEditTask = true;
+    taskInput.value = textName;
+    taskInput.focus();
+    taskInput.classList.add("active");
+}
+
+function deleteTask(deleteId, filter) {
+    isEditTask = false;
+    todos.splice(deleteId, 1);
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo(filter);
+}
+
+clearAll.addEventListener("click", () => {
+    isEditTask = false;
+    todos.splice(0, todos.length);
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo()
+});
+
+taskInput.addEventListener("keyup", e => {
+    let userTask = taskInput.value.trim();
+    if (e.key == "Enter" && userTask) {
+        if (!isEditTask) {
+            todos = !todos ? [] : todos;
+            let taskInfo = { name: userTask, status: "pending" };
+            todos.push(taskInfo);
+        } else {
+            isEditTask = false;
+            todos[editId].name = userTask;
+        }
+        taskInput.value = "";
+        localStorage.setItem("todo-list", JSON.stringify(todos));
+        showTodo(document.querySelector("span.active").id);
     }
 });
