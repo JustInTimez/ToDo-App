3ba6fa7cafea9ba9e5bb03297f6b86c56ca3b3e3
JustInTimez <just.oost@gmail.com>
2022-09-08 14:51:46 +0200

    Comments, typewriting effect
    Function created with intent of adding a bit more interactivity to the page. This function runs a typewriter style with predetermined text, looping

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,8 +1,56 @@
+// --------------------------------------------------------------------------
+//                                  SET VARIABLES
+// --------------------------------------------------------------------------
 const taskInput = document.getElementById("taskInput");
 const filters = document.querySelectorAll(".filters span");
 const clearAll = document.querySelector(".clear-btn");
 const taskBox = document.querySelector(".task-box");
 const dateInput = document.getElementById("task-date");
+// --------------------------------------------------------------------------
+//                                  PRESENTATION
+// --------------------------------------------------------------------------
+
+// ----------------------------Navbar Typewriter Effect---------------------
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+const textArray = ["Plan Your Day!", "Plan Your Week!", "Plan Your Month!"];
+const typingDelay = 100;
+const erasingDelay = 100;
+const newTextDelay = 700;      // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+};
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 100);
+});
 
 let editId,
     isEditTask = false,
@@ -73,7 +121,9 @@ function updateStatus(selectedTask) {
     localStorage.setItem("todo-list", JSON.stringify(todos))
 }
 
-// To-Do functionality starts here
+// --------------------------------------------------------------------------
+//                                  APP LOGIC
+// --------------------------------------------------------------------------
 
 function editTask(taskId, textName, dateName) {
     editId = taskId;
@@ -150,4 +200,4 @@ function createTask() {
     taskInput.value = "";
     localStorage.setItem("todo-list", JSON.stringify(todos));
     showTodo(document.querySelector("span.active").id);
-}
+};
